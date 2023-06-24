f59956d9f48ac40b9ba8dfe7450a4a16fb98fe34
Naiane Pinheiro Reis <email.annereis@gmail.com>
2023-06-24 15:08:04 -0300

    feat: create pagination db

--- front-end/src/components/button/styles.ts
+++ front-end/src/components/button/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 export const Container = styled.button<{ types: string }>`
+  ${tw` bg-transparent text-blue-tertiary  border-2 border-solid  border-blue-tertiary text-blue-tertiary p-2 font-semibold cursor-pointer rounded-3xl`}
   ${({ types }) =>
     types == "primary" && tw`bg-transparent border-2 border-solid text-sm border-blue-primary text-blue-primary p-2 cursor-pointer rounded-sm `}
 `
--- front-end/src/components/filter/styles.ts
+++ front-end/src/components/filter/styles.ts
@@ -2,5 +2,16 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 export const Container = styled.div`
- ${tw` w-full mx-auto flex justify-center items-center h-32`}
+ ${tw` w-full mx-auto flex justify-center items-center h-32 justify-between max-w-6xl`}
+
+ div{
+    ${tw` flex justify-center items-center gap-x-4`}
+ }
+`
+export const Select = styled.select`
+  ${tw` bg-transparent text-blue-tertiary  border-2 border-solid  border-blue-tertiary text-blue-tertiary p-3 font-semibold cursor-pointer rounded-xl`}
+`
+
+export const Label = styled.label`
+  ${tw` bg-transparent text-blue-tertiary text-blue-tertiary font-extrabold `}
 `
